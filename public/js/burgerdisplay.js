c40f11a006b061f14f8410bc4b43854ab87444d6
jzubak <jzubak81@gmail.com>
2019-05-13 18:23:23 -0400

    monday 1

--- public/js/burgerdisplay.js
+++ public/js/burgerdisplay.js
@@ -2,10 +2,10 @@
 $(function () {
     $(".devoured").on("click", function (event) {
         var id = $(this).data("id");
-        var eaten = $(this).data("neweaten");
+        var neweaten = $(this).data("neweaten");
 
         var newDevoured = {
-            devoured: eaten
+            devoured: neweaten
         };
 
         // Send the PUT request.
@@ -14,7 +14,7 @@ $(function () {
             data: newDevoured
         }).then(
             function () {
-                console.log("changed devoured status to ", eaten);
+                console.log("changed devoured status to ", neweaten);
                 // Reload the page to get the updated list
                 location.reload();
             }
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var express = require("express");
-
-var app = express();
 var PORT = process.env.PORT || 3000;
+var app = express();
 
-var db = require("./models")
+app.use(express.static("public"));
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
-
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" , partialsDir: __dirname + './views/partials/burgers'}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" 
+, partialsDir: __dirname + './views/partials'
+}));
 app.set("view engine", "handlebars");
 // Handlebars.registerPartial('burger/burger-block', '{{burger-block}}')
 
@@ -20,11 +20,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
-// // db.sequelize.sync({ force: true }).then(function() {
-//   app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-// });
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
