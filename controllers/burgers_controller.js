3070a11eae67f24c26fa6d1776c51e0072745df4
jzubak <jzubak81@gmail.com>
2019-05-13 19:03:49 -0400

    heroku inital commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,6 +5,12 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+
+router.get('/', function (req, res) {
+    res.redirect('/index');
+  });
+
+  
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
     burger.all(function (data) {
